2a125bfe4737db87f2380a6fc8f65c2c51851b22
Petr Filaretov <p.filaretov@okko.tv>
2024-01-25 10:24:07 +0300

    initial commit

---
+++ src/main/java/dev/pfilaretov42/hitchhikers/guide/HitchhikersGuideApplication.java
@@ -0,0 +1,13 @@
+package dev.pfilaretov42.hitchhikers.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitchhikersGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitchhikersGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pfilaretov42/hitchhikers/guide/HitchhikersGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pfilaretov42.hitchhikers.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitchhikersGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
